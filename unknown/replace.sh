41d111f128c99b3cab311bdb773de56e449d0aae
Cong Li <cong.li@inf.ethz.ch>
2025-06-05 00:17:07 +0200

    cleanup

--- unknown/replace.sh
+++
@@ -1,8 +0,0 @@
-for set in {1..3}; do
-  for i in {1..10}; do
-    sed -i \
-      -e 's/bool enableConsistentWalks = true;/bool enableConsistentWalks = false;/' \
-      -e 's/const bool enableRandomInitialisations = true;/const bool enableRandomInitialisations = false;/' \
-      /local/home/kchopra/coeff_set_"$set"/copy_"$i"/params.hpp
-  done
-done
